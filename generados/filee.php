1f3e73e9902732f45870b0d40e1a732628aafeec
Nesulises7 <ne$tne$tor13652nlima@lieison.com>
2015-07-27 13:47:46 -0600

    txtgenerador

--- generados/filee.php
+++ generados/filee.php
@@ -27,59 +27,18 @@
         <script src="..(js/fileinput.min.js" type="text/javascript"></script>
 </head>
 <body>
-<div class="page-header">
 <div class="page-container">
 	<div class="page-head">
-		<div class="container">
-			<div class="page-title">
-				<h1>Generar Archivo</h1>
-			</div>
-			<div class="page-toolbar">
-				<div class="btn-group btn-theme-panel">
-					<a href="javascript:;" class="btn dropdown-toggle" data-toggle="dropdown">
-					<i class="icon-settings"></i>
-					</a>
-					<div class="dropdown-menu theme-panel pull-right dropdown-custom hold-on-click">						
-			</div>
-		</div>
 	</div>
 	<div class="page-content">
 		<div class="container">
-			<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-							<h4 class="modal-title">Modal title</h4>
-						</div>
-						<div class="modal-body">
-							 Widget settings form goes here
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn blue">Save changes</button>
-							<button type="button" class="btn default" data-dismiss="modal">Close</button>
-						</div>
-					</div>
-				</div>
-			</div>
-					<div class="row">
+			<div class="row">
 				<div class="col-md-12">
 					<div class="portlet light" id="form_wizard_1">
 						<div class="portlet-title">
 							<div class="caption">
 								<span class="caption-subject font-green-sharp bold uppercase">
-								<i class="fa fa-gift"></i> Generar Archivo
-								</span>
-							</div>
-							<div class="tools">
-								<a href="javascript:;" class="collapse">
-								</a>
-								<a href="#portlet-config" data-toggle="modal" class="config">
-								</a>
-								<a href="javascript:;" class="reload">
-								</a>
-								<a href="javascript:;" class="remove">
-								</a>
+								Generador de archivos .txt</span>
 							</div>
 						</div>
 						<div class="portlet-body form">
@@ -87,9 +46,9 @@
 									<div class="form-body">
 										<ul class="nav nav-pills nav-justified steps">
 											<li>
-												<a href="#tab1" data-toggle="tab" class="step">
+												<a data-toggle="tab" class="step">
 												<span class="number">
-												1 </span>
+												2 </span>
 												<span class="desc">
 												<i class="fa fa-check"></i>Archivo Generado</span>
 												</a>
@@ -101,58 +60,48 @@
 										</div>
 										<div class="tab-content">
 											<div class="alert alert-danger display-none">
-												<button class="close" data-dismiss="alert"></button>
-												You have some form errors. Please check below.
+												
 											</div>
 											<div class="alert alert-success display-none">
-												<button class="close" data-dismiss="alert"></button>
-												Your form validation is successful!
+												
 											</div>
-											<div class="tab-pane active" id="tab2">
-												<h3 class="block">Archivo Generado</h3>
-												<div class="form-group" align="center">
-													<div class="container">
+											<div class="tab-pane active" id="tab1">
+												<h3 class="block">,Lista de Archivos Generados</h3>
+												<div class="form-group">
+												</div>
+										</div>
+									</div>
+									<div class="form-actions">
+										<div class="row"><center>
 													    <?php
 															$archivo =  $_FILES['archivo']['tmp_name'];
 															$data = file_get_contents($archivo);
 															$array_ = explode(",", $data);
-															echo "El numero de corres generados es:",sizeof($array_);
-															$file_create = $_FILES['archivo']['name'] . rand(200 ,20000) . ".txt";
+															echo "El Numero de Correos Generados es:",sizeof($array_);
+															$file_create = $_FILES['archivo']['name'] . rand(200 ,20000) .".txt";
 															$handle = fopen( $file_create, 'w');
 															foreach ($array_ as $value){
 															    fwrite($handle, "$value\n");
 															}
 															fclose($handle);
 													  	?>
-													  	<br>
-													  	<input type="button" onclick="history.back()" class="btn-primary" name="volver atrás" value="volver atrás"><br>
+													  	<br><br>
+													  	<a href="../index.html" class="btn btn-lg btn-primary">Atras</a><br><br><br>
 <?php
 	$listar = null;
 	$directorio = opendir("../generados/");
 	while ($elemento = readdir($directorio)) {
 		if (!is_dir($elemento)) {
 			if($elemento != "filee.php") 
-				$listar .="<li><a href='$elemento' target='_blank'>$elemento/</a></li>";
+				$listar ="<li><a href='$elemento' class='btn btn-default btn-lg active' target='_blank'>$elemento/</a></li>";
 		}
 	}
-
 ?>
 													  	<ul>
 													  		<?php echo $listar; ?>
 
-													  	</ul>
+													  	</ul></center>
 											       	</div>
-												</div>
-											</div>
-											<div class="tab-pane" id="tab2">
-												<h3 class="block">Provide your profile details</h3>
-												<div class="form-group">
-													<label class="control-label col-md-3">Fullname <span class="required">
-													* </span>
-													</label>
-												</div>
-											</div>
-										</div>
 									</div>
 								</div>
 						</div>
@@ -162,30 +111,5 @@
 		</div>
 	</div>
 </div>
-<div class="page-footer">
-	<div class="container">
-	</div>
-</div>
-<div class="scroll-to-top">
-	<i class="icon-arrow-up"></i>
-</div>
-<script src="../js/jquery.min.js" type="text/javascript"></script>
-<script src="../js/jquery-migrate.min.js" type="text/javascript"></script>
-<script src="../js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-<script src="../js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../js/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="../js/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="../js/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="../js/jquery.cokie.min.js" type="text/javascript"></script>
-<script src="../js/jquery.uniform.min.js" type="text/javascript"></script>
-<script type="../js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="../js/additional-methods.min.js"></script>
-<script type="text/javascript" src="../js/jquery.bootstrap.wizard.min.js"></script>
-<script type="text/javascript" src="../js/select2.min.js"></script>
-<script src="../js/javascript"></script>
-<script src="../js/layout.js" type="text/javascript"></script>
-<script src="../js/demo.js" type="text/javascript"></script>
-<script src="../js/form-wizard.js"></script>
-</script>
 </body>
 </html>
